e3fb7c38efda8aa6c3b65d79e891cbf2caef684a
erichVK5 <a1039181@gmail.com>
2022-04-23 01:57:57 +0930

    Implement Inkscape v1.1 compatibility
    Tested on Inkscape v1.1 but not Inkscape v1.0

--- pcb_rnd_output.py
+++ pcb_rnd_output.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 '''
-Copyright (C) 2017-2020 Erich Heinzle a1039181@gmail.com
+Copyright (C) 2017-2022 Erich Heinzle a1039181@gmail.com
 based on GPL HPGL export code by Aaron Spike, aaron@ekips.org
 
 This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
-import inkex, simpletransform, cubicsuperpath, simplestyle, cspsubdiv, random
+import inkex, random
+from inkex.paths import CubicSuperPath, Path
+from inkex.transforms import Transform
+from inkex.bezier import cspsubdiv
 
 # we only want to export simple polygons to pcb-rnd
 # so we need a test that determines if a path is a simple
@@ -74,7 +77,7 @@ class Point:
     def equals(self, P):
         return ((self.x == P.x) and (self.y == P.y))
 
-class MyEffect(inkex.Effect):
+class MyEffect(inkex.EffectExtension):
 
     header = "li:pcb-rnd-subcircuit-v7 {\n ha:subc.74 {\n  ha:attributes {\n   refdes = U0\n  }\n  ha:data {\n   li:padstack_prototypes {\n   }\n   li:objects {\n   }\n   li:layers {\n"
     pre_polygon_layer_defs = '''    ha:top-sig {\n     lid=0\n     ha:type {\n      copper = 1\n      top = 1\n     }\n     li:objects {\n'''
@@ -100,39 +103,38 @@ class MyEffect(inkex.Effect):
     lineCount = 1
     polygonCount = 1
     
-    def __init__(self):
-        inkex.Effect.__init__(self)
-        self.OptionParser.add_option("-f", "--flatness",
-                        action="store", type="float", 
+    def add_arguments(self, pars):
+        pars.add_argument("-f", "--flatness",
+                        type=float, 
                         dest="flat", default=0.2,
                         help="Minimum flatness of the subdivided curves")
-        self.OptionParser.add_option("-m", "--mirror",
-                        action="store", type="inkbool", 
+        pars.add_argument("-m", "--mirror",
+                        type=inkex.Boolean, 
                         dest="mirror", default="TRUE",
                         help="Mirror Y-Axis")
-        self.OptionParser.add_option("-x", "--xOrigin",
-                        action="store", type="float", 
+        pars.add_argument("-x", "--xOrigin",
+                        type=float, 
                         dest="xOrigin", default=0.0,
                         help="X Origin (pixels)")
-        self.OptionParser.add_option("-y", "--yOrigin",
-                        action="store", type="float", 
+        pars.add_argument("-y", "--yOrigin",
+                        type=float, 
                         dest="yOrigin", default=0.0,
                         help="Y Origin (pixels)")
-        self.OptionParser.add_option("-r", "--resolution",
-                        action="store", type="int", 
-                        dest="resolution", default=1000,
-                        help="Resolution (dpi)")
-        self.OptionParser.add_option("-t", "--thickness",
-                        action="store", type="int",
+        pars.add_argument("-s", "--scaling",
+                        type=float, 
+                        dest="scaling", default=1.0,
+                        help="Scaling")
+        pars.add_argument("-t", "--thickness",
+                        type=int,
                         dest="thickness", default=8,
                         help="Line thickness")
-        self.OptionParser.add_option("-p", "--plotInvisibleLayers",
-                        action="store", type="inkbool", 
+        pars.add_argument("-p", "--plotInvisibleLayers",
+                        type=inkex.Boolean, 
                         dest="plotInvisibleLayers", default="FALSE",
                         help="Plot invisible layers")
 
-    def output(self):
-        print ''.join(self.lht)
+    def save(self, stream):
+        stream.write(''.join(self.lht).encode('utf-8'))
 
     def export_polygon(self, total_path):
         self.polygons.append('        ha:polygon.%d {\n' % (self.polygonCount))
@@ -150,32 +152,29 @@ class MyEffect(inkex.Effect):
         self.polygonCount = self.polygonCount + 1
 
 # here we process a cubic super path, and export it as lines on the top silk layer
-    def process_path(self, node, mat):
-        d = node.get('d')
+
+    def process_path(self, node, transform):
+        path = node.path.to_absolute()\
+                   .transform(node.composed_transform())\
+                   .transform(transform)\
+                   .to_superpath()
         total_path = []
-        if d:
-            p = cubicsuperpath.parsePath(d)
-            trans = node.get('transform')
-            if trans:
-                mat = simpletransform.composeTransform(mat, simpletransform.parseTransform(trans))
-            simpletransform.applyTransformToPath(mat, p)
-            cspsubdiv.cspsubdiv(p, self.options.flat)
-            for sp in p:
-                first = True
-		X = -1
-		Y = -1
-                for csp in sp:
-                    X = csp[1][0]
-                    Y = csp[1][1]
-                    total_path.append(Point(X, Y))
+        if path:
+            cspsubdiv(path, self.options.flat)
+            first = True
+            oldPosX = -1
+            oldPosY = -1
+            for singlePath in path:
+                for singlePathPoint in singlePath:
+                    posX, posY = singlePathPoint[1]
+                    total_path.append(Point(posX, posY))
                     if not first:
                         self.lines.append('        ha:line.%d {\n' % (self.lineCount))
-			self.lineCount = self.lineCount + 1
-                        self.lines.append('         x1=%dmil; y1=%dmil; x2=%dmil; y2=%dmil; thickness=%dmil; clearance=40.0mil;\n' % (Xlast/100.0, Ylast/100.0, X/100.0, Y/100.0, self.options.thickness))
+                        self.lineCount = self.lineCount + 1
+                        self.lines.append('         x1=%dmil; y1=%dmil; x2=%dmil; y2=%dmil; thickness=%dmil; clearance=40.0mil;\n' % (oldPosX/100.0, oldPosY/100.0, posX/100.0, posY/100.0, self.options.thickness))
                         self.lines.append('         ha:flags {\n          clearline=1\n         }\n        }\n')
-
-                    Xlast = X
-                    Ylast = Y
+                    oldPosX = posX
+                    oldPosY = posY
                     first = False
         # here, we test if the path we just processed defines a simple polygon, i.e. one with no
         # self intersections. If so, we export it as a polygon as well, on the top copper layer.
@@ -183,23 +182,15 @@ class MyEffect(inkex.Effect):
             self.export_polygon(total_path)
 
     def process_group(self, group):
-        style = group.get('style')
-        if style:
-            style = simplestyle.parseStyle(style)
-            if style.has_key('display'):
-                if style['display']=='none':
-                    if not self.options.plotInvisibleLayers:
-                        return
-        trans = group.get('transform')
-        if trans:
-            self.groupmat.append(simpletransform.composeTransform(self.groupmat[-1], simpletransform.parseTransform(trans)))
-        for node in group:
-            if node.tag == inkex.addNS('path','svg'):
-                self.process_path(node, self.groupmat[-1])
-            if node.tag == inkex.addNS('g','svg'):
-                self.process_group(node)
-        if trans:
-            self.groupmat.pop()
+        """flatten layers and groups to avoid recursion"""
+        for child in group:
+            if not isinstance(child, inkex.ShapeElement):
+                continue
+            if child.is_visible():
+                if isinstance(child, inkex.Group):
+                    self.process_group(child)
+                elif isinstance(child, inkex.PathElement):
+                    self.process_path(child, Transform(self.groupmat))
 
     def effect(self):
         self.lht = ['# pcb-rnd v7 subcircuit exported from Inkscape\n']
@@ -209,14 +200,14 @@ class MyEffect(inkex.Effect):
         self.polygons = ['']
         x0 = self.options.xOrigin
         y0 = self.options.yOrigin
-        scale = float(self.options.resolution)/10
+        scale = float(self.options.scaling)
         self.options.flat *= scale
         mirror = 1.0
         if self.options.mirror:
             mirror = -1.0
-            if inkex.unittouu(self.document.getroot().xpath('@height', namespaces=inkex.NSS)[0]):
-                y0 -= float(inkex.unittouu(self.document.getroot().xpath('@height', namespaces=inkex.NSS)[0]))
-        self.groupmat = [[[scale, 0.0, -x0*scale], [0.0, mirror*scale, -y0*scale]]]
+            if self.svg.unittouu(self.document.getroot().xpath('@height', namespaces=inkex.NSS)[0]):
+                y0 -= float(self.svg.unittouu(self.document.getroot().xpath('@height', namespaces=inkex.NSS)[0]))
+        self.groupmat = [[scale, 0.0, 0.0], [0.0, mirror*scale, 0.0]]
         doc = self.document.getroot()
         self.process_group(doc)
         self.lht.append(''.join(self.polygons))
@@ -226,8 +217,6 @@ class MyEffect(inkex.Effect):
         self.lht.append('# here endeth the lihata')
 
 if __name__ == '__main__':   #pragma: no cover
-    e = MyEffect()
-    e.affect()
-
+    MyEffect().run()
 
 # vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 encoding=utf-8 textwidth=99
